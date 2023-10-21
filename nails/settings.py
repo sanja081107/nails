d0323f30a6ed697eb64769432892f79b3d0e133c
new user <sanja081107@gmail.com>
2023-10-21 17:19:51 +0300

    added yandex and vk auth

--- nails/settings.py
+++ nails/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.yandex',
+    'allauth.socialaccount.providers.vk',
 ]
 
 MIDDLEWARE = [
